edee0114baa39b796b9524ffb8cafe2a2e679a68
Ronald Abu Saleh <lancerrnld@gmail.com>
2022-09-25 20:51:56 -0400

    Added database + changes

--- src/database/sql-queries.sql
+++ src/database/sql-queries.sql
@@ -7,7 +7,11 @@ SELECT * FROM product;
 SELECT p.IDProduct as productId, p.Name as productName, p.SKU as sku, p.Quantity as quantity, p.Price as price, v.Name as vendorName FROM product as p INNER JOIN vendor as v ON p.IDVendor = v.IDVendor;
 SELECT p.IDProduct as productId, p.Name as productName, p.SKU as sku, p.Quantity as quantity, p.Price as price, v.Name as vendorName FROM product as p INNER JOIN vendor as v ON p.IDVendor = v.IDVendor WHERE v.IDVendor IN (1, 2);
 
-DESCRIBE product;
+SELECT * FROM product WHERE Name LIKE '%' AND SKU LIKE '%' AND Price >= 4 AND Price <= 5;
+
+SELECT p.IDProduct as productId, p.Name as productName, p.SKU as sku, p.Quantity as quantity, p.Price as price, v.Name as vendorName FROM product as p INNER JOIN vendor as v ON p.IDVendor = v.IDVendor WHERE (p.Name LIKE "%" OR p.SKU LIKE "%") AND (p.Price >= 4 AND p.Price <= 5);
+
+DESCRIBE admin;
 
 SET FOREIGN_KEY_CHECKS=0;
 TRUNCATE vendor;
