4be7914fe441b7c630df9eea9830abdecc48fbd3
Ronald Abu Saleh <lancerrnld@gmail.com>
2022-09-25 20:38:31 -0400

    Products by params function is changed

--- src/route/buyer/buyer.js
+++ src/route/buyer/buyer.js
@@ -2,17 +2,17 @@ const query = require('../../database')
 
 exports.productsByParams = async (params) => {
   try {
-    const { name, sku, min, max } = params
+    const { byText, min, max } = params
     
-    let sql          = `SELECT * FROM product WHERE Name LIKE ? AND SKU LIKE ?`
-    const bindParams = [`${name}%`, `${sku}%`]
+    let sql          = `SELECT p.IDProduct as productId, p.Name as productName, p.SKU as sku, p.Quantity as quantity, p.Price as price, v.Name as vendorName FROM product as p INNER JOIN vendor as v ON p.IDVendor = v.IDVendor WHERE (p.Name LIKE ? OR p.SKU LIKE ?)`
+    const bindParams = [`${byText}%`, `${byText}%`]
 
     if (min) {
         sql += ' AND Price >= ?'
         bindParams.push(min)
     }
 
-    if (max) {
+    if (max || max === 0) {
       sql += ' AND Price <= ?'
       bindParams.push(max)
     }
